54e66c19762623f715177b75a9d92fc012862a89
Ivo Dolenc <ivodolenc@gmail.com>
2025-02-18 00:51:36 +0100

    chore: update playground

--- playground/types.ts
+++ playground/types.ts
@@ -1,6 +1,6 @@
 import type { Merge } from '@/types'
 
-type A = {
+type A1 = {
   a: boolean
   b: {
     c: {
@@ -12,7 +12,7 @@ type A = {
   }
 }
 
-type B = {
+type B1 = {
   a: string
   b: {
     c: {
@@ -27,23 +27,62 @@ type B = {
   }
 }
 
-type C = {
+type C1 = {
   i: {
     j: number
     k: number
   }
 }
 
-type D = {
+type D1 = {
   i: {
     j?: undefined
     k?: null
   }
 }
 
-export type Result = Merge<[A, B, C, D]>
+export type ResultDefault = Merge<[A1, B1, C1, D1]>
 
 export type ResultRules = Merge<
-  [A, B, C, D],
-  { rules: { array: 'override'; undefined: 'skip'; null: 'skip' } }
+  [A1, B1, C1, D1],
+  {
+    rules: { array: 'override'; undefined: 'skip'; null: 'skip' }
+  }
 >
+
+type A2 = {
+  a: boolean[]
+  b: {
+    c: {
+      d: string
+      e: {
+        f: number
+      }
+    }
+  }
+  d: boolean
+  e: {
+    f: string
+    g: number[]
+  }
+}
+
+type B2 = {
+  a?: string[]
+  b: {
+    a: number
+  }
+  d: number
+  e: {
+    f: number[]
+    g?: string
+  }
+}
+
+type C2 = {
+  i?: string | null
+}
+
+export type ResultDepth0 = Merge<[A2, B2, C2], { depth: 0 }>
+
+export type ResultDepth1 = Merge<[A2, B2, C2], { depth: 1 }>
